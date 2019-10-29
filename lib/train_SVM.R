fadcf40f3cfc780b1f17f012a6b878bd3e6f0af9
yw3211 <49082938+yw3211@users.noreply.github.com>
2019-10-29 16:43:21 -0400

    Add files via upload

---
+++ lib/train_SVM.R
@@ -0,0 +1,27 @@
+###########################################################
+### Train a classification model with training features ###
+###########################################################
+train <- function(feature_df = pairwise_data, par = NULL){
+  ### Train an SVM model using processed features from training images
+  
+  ### Input:
+  ### - a data frame containing features and labels
+  ### - a parameter list
+  ### Output: trained model
+  
+  ### load libraries
+  library("e1071")
+  
+  ### Train with SVM
+  if(is.null(par)){
+    cost = 1
+  } else {
+    cost = par$cost
+  }
+  
+   svm_model <- svm(emotion_idx~., data = feature_df,
+                    kernel = "linear", cost = cost) 
+
+  return(model = svm_model)
+}
+
