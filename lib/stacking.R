2b027d50d105ae9535c808d936289b5c9ede9e87
sh3799 <haosixing@hotmail.com>
2019-10-31 12:47:41 -0400

    Update stacking.R

--- lib/stacking.R
+++ lib/stacking.R
@@ -1,19 +1,28 @@
-stacking <- function(xgb_model, svm_model, coef = rep(1/2,2), testdata){
+stacking <- function(xgb_model, svm_model, coef = rep(1/2,2), testdata, nolabel=FALSE){
   
   library(dplyr)
   library(caret)
   source("../lib/xgb_test.R")
   
-  xgb.pred <- xgb_test(xgb_model, testdata)[,-1]
+  if(nolabel==FALSE){
+    xgb.pred <- xgb_test(xgb_model, testdata)[,-1]
+  }else{
+    xgb.pred <- xgb_test(xgb_model, testdata[,c(1:(length(dat_train) - 1))])[,-1]
+  }
+  
+  
   svm.pred <- attr(svm_test(svm_model, testdata, probability=TRUE), "probabilities")
   svm.pred <- svm.pred[, order(as.integer(colnames(svm.pred)))]
   
   pred.stack <- coef[1]*xgb.pred + coef[2]*svm.pred %>% as.data.frame()
   pred.stack$prediction <- max.col(pred.stack)
-  pred.stack$label <- testdata$emotion_idx
   
-  pred.stack.conf <- confusionMatrix(factor(pred.stack$prediction), factor(pred.stack$label), mode = "everything")
-  
-  return(pred.stack.conf)
+  if(nolabel==FALSE){ 
+    pred.stack$label <- testdata$emotion_idx
+    pred.stack.conf <- confusionMatrix(factor(pred.stack$prediction), factor(pred.stack$label), mode = "everything")
+    return(pred.stack.conf)
+  }else{
+     return(pred.stack$prediction) 
+    }
   
 }
