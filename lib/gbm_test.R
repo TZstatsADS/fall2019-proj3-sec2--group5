3d805101e6bd5f56bf2ebf4e4d1c5a2593f478dc
Sagar Lal <sagar.lal@columbia.edu>
2019-10-31 14:35:14 -0400

    Cleaning up repository

--- lib/gbm_test.R
+++ lib/gbm_test.R
@@ -9,7 +9,7 @@ gbm_test<-function(model, dat_test){
 	###Testing
 	tm.test<-system.time(pred_gbm<-predict(object=model,
                   newdata=dat_test,
-                  n.trees=best_iter,
+                  n.trees=best_iter, missing=NA,
                   type="response"))
 	
 	return(list(pred_gbm,tm.test))
